69e595afcbcbc493bf1e6a287060bdf78877499d
Claudio Schiminsky Junior <schiminsky@hotmail.com>
2026-02-28 18:47:23 -0300

    gh pages

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,10 +1,9 @@
 import './Banner.scss';
-import BannerImg from '../../assets/images/banner.png';
 
 function Banner() {
   return (
     <section className="main-banner-container">
-      <img src={BannerImg} alt="Banner" />
+      <img src="/assets/images/banner.png" alt="Banner" />
     </section>
   );
 }
